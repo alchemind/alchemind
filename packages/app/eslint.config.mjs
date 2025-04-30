abbc2837fbdaac18988cc0579e58e1d73d579057
Peter Bartha <peterbartha@pm.me>
2025-05-01 00:17:54 +0200

    chore: set up commit hook to enforce code formatting

--- packages/app/eslint.config.mjs
+++ packages/app/eslint.config.mjs
@@ -1,8 +1,8 @@
-import { eslintConfigFactory } from '@alchemind/tooling/eslint-svelte';
-import { includeIgnoreFile } from '@eslint/compat';
-import { fileURLToPath } from 'node:url';
-import svelteConfig from './svelte.config.js';
+import { eslintConfigFactory } from "@alchemind/tooling/eslint-svelte";
+import { includeIgnoreFile } from "@eslint/compat";
+import { fileURLToPath } from "node:url";
+import svelteConfig from "./svelte.config.js";
 
-const gitignorePath = fileURLToPath(new URL('./.gitignore', import.meta.url));
+const gitignorePath = fileURLToPath(new URL("./.gitignore", import.meta.url));
 
 export default [...eslintConfigFactory(svelteConfig), includeIgnoreFile(gitignorePath)];
